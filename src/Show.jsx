d7571ccd1042d091adebe3ae2e33592175fd355a
Anthony Gibah <gibahanthony@gmail.com>
2024-05-05 01:10:13 +0100

    Testing

---
+++ src/Show.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+function Show(){
+    return(
+        <div>
+            <h2 style={{alignContent: 'center'}}>
+                EDITED
+            </h2>
+        </div>
+    );
+}
+
+export default Show;
